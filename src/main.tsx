11b9b19b1f861c6e749e596f5898328e17b1b590
danupon_th <notedanupon@gmail.com>
2025-09-18 16:36:03 +0700

    R8

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,20 @@
-﻿import React from "react";
-import ReactDOM from "react-dom/client";
+﻿// src/main.tsx
+import React from "react";
+import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+function ensureRoot(): HTMLElement {
+  let el = document.getElementById("root");
+  if (!el) {
+    el = document.createElement("div");
+    el.id = "root";
+    document.body.appendChild(el);
+  }
+  return el as HTMLElement;
+}
+
+const container = ensureRoot();
+createRoot(container).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
